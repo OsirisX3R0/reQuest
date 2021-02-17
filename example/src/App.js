bde317b89dbf15a48a587b3c1fd6567270115955
Richard Harris <richardharris916@gmail.com>
2021-02-17 11:35:01 -0600

    Updated example

--- example/src/App.js
+++ example/src/App.js
@@ -5,17 +5,18 @@ import './css/style.css'
 const App = () => {
     const [person, personLoading, personError, getPerson] = useHttp('https://swapi.dev/api/people/1', {returnJson: true})
 
-    const displayData = personLoading
-        ? 'Loading'
-        : personError
-            ? personError
-            : person && <pre>{JSON.stringify(person)}</pre>
-
-    return (
-        <div className='container'>
-            {displayData}
-        </div>
+    const displayData = (
+        <pre className='container'>
+            {personLoading
+                ? 'Loading'
+                : personError
+                    ? personError
+                    : person && JSON.stringify(person)}
+                    
+        </pre>
     )
+
+    return displayData
 }
 
 export default App
